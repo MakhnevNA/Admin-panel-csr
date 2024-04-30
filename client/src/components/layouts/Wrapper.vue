1c6a34917956e9d8b1791f45ecb73bf2a9d35e65
Nikita <nikitka.makhnev@mail.ru>
2024-04-30 22:42:13 +0500

    Убрал header из UI

--- client/src/components/layouts/Wrapper.vue
+++ client/src/components/layouts/Wrapper.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script setup lang="ts">
-import Header from '@/components/UI/header/Header.vue';
+import Header from '@/components/header/Header.vue';
 </script>
 
 <style scoped lang="scss">
